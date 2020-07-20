7760c8dd3663cc4a41c0b3c0ad2b1f3a2a84db29
darshan bordekar <darshanbordekar1819@gmail.com>
2020-07-21 02:04:38 +0530

    [ Darshan ] | Phase 1 | Basic Structure Created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [amount, setAmount] = useState("");
+  const [breakDown, setBreakDown] = useState("");
+  const [part, setPart] = useState([]);
+  const getAmt = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const getBreakDown = (e) => {
+    setBreakDown(e.target.value);
+  };
+
+  const getTotalBreakDown = (amount, breakDown) => {
+    let installment = Number(amount) / Number(breakDown);
+    let equalBreakDown = [...Array(Number(breakDown))].map((data) => {
+      return installment;
+    });
+    setPart(equalBreakDown);
+    return part;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log(amount, breakDown, part)}
+      <input value={amount} onChange={getAmt}></input>
+      <input value={breakDown} onChange={getBreakDown}></input>
+      <button onClick={() => getTotalBreakDown(amount, breakDown)}>
+        calculate
+      </button>
+      {part.map((data) => (
+        <div>
+          <span>amount to pay {data}</span>
+          <input></input>
+          {/* on pay button 4-5 condition */}
+          <button>Pay</button>
+        </div>
+      ))}
     </div>
   );
 }
