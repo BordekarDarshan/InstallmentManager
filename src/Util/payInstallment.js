8342ad4d0bf397cfdea1533dd30f4dfe8fc4928c
darshan bordekar <darshanbordekar1819@gmail.com>
2020-07-23 19:54:51 +0530

    [ Darshan ]

--- src/Util/payInstallment.js
+++ src/Util/payInstallment.js
@@ -14,19 +14,25 @@ export const payInstallment = (
         if (advance > next.installment) {
           for (let index = 0; index < installmentStructure.length; index++) {
             if (index !== installmentStructure.length - 1) {
-              let next = installmentStructure[index + 1];
               let current = installmentStructure[index];
+              let next = installmentStructure[index + 1];
+
               let adv = current.value - current.installment;
-              next.installment = adv - next.installment;
-              if (next.installment > 0) {
-                next.value = next.installment;
-                next.installment = 0;
-              } else {
-                next.installment = next.installment - advance;
+              // 5
+              console.log(adv);
+              if (adv > 0) {
+                //true
+                next.value = adv;
+                next.installment = next.value - next.installment;
+
+                console.log(adv);
+                setInstallmentStructure([...installmentStructure]);
               }
 
-              setInstallmentStructure([...installmentStructure]);
-              console.log("adv", adv, "next", next.installment);
+              // if (current.installment > 0) {
+              //   next.installment = current.installment + 1;
+              // }
+              console.log("current", current, "next", next);
             }
           }
         } else {
