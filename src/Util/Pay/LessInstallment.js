b2cffa2139150954640590ab2370f521401584db
darshan bordekar <darshanbordekar1819@gmail.com>
2020-08-10 17:44:26 +0530

    [ Darshan ] | Less Installment | Refactor

--- src/Util/Pay/LessInstallment.js
+++ src/Util/Pay/LessInstallment.js
@@ -11,40 +11,77 @@ export function lessInstallment(
 ) {
   if (lessInstallmentFeature === "adjust") {
     if (index === installmentStructure.length - 1) {
-      let remain = content.installment - content.value;
-
-      installmentStructure.splice(i, 1);
-      setInstallmentStructure([
-        ...installmentStructure,
-        {
-          id: installmentStructure.length,
-          installment: remain,
-          value: 0,
-        },
-      ]);
-
-      setPaidInstallment([...paidInstallment, { ...content }]);
+      createNewInstallment(
+        content,
+        installmentStructure,
+        setInstallmentStructure,
+        i,
+        setPaidInstallment,
+        paidInstallment
+      );
     } else {
-      let elementNext = next[i + 1];
-
-      let remain = content.installment - content.value;
-      elementNext.installment = elementNext.installment + remain;
-      installmentStructure.splice(i, 1);
-      setInstallmentStructure([...installmentStructure]);
-      setPaidInstallment([...paidInstallment, { ...content }]);
+      addjustInNextInstallment(
+        next,
+        content,
+        installmentStructure,
+        setInstallmentStructure,
+        i,
+        setPaidInstallment,
+        paidInstallment
+      );
     }
   }
   if (lessInstallmentFeature === "createNew") {
-    let remain = content.installment - content.value;
-    installmentStructure.splice(i, 1);
-    setInstallmentStructure([
-      ...installmentStructure,
-      {
-        id: installmentStructure.length,
-        installment: remain,
-        value: 0,
-      },
-    ]);
-    setPaidInstallment([...paidInstallment, { ...content }]);
+    createNewInstallment(
+      content,
+      installmentStructure,
+      setInstallmentStructure,
+      i,
+      setPaidInstallment,
+      paidInstallment
+    );
   }
+}
+
+// Create New Installment Feature
+function createNewInstallment(
+  content,
+  installmentStructure,
+  setInstallmentStructure,
+  i,
+  setPaidInstallment,
+  paidInstallment
+) {
+  let remain = content.installment - content.value;
+
+  installmentStructure.splice(i, 1);
+  setInstallmentStructure([
+    ...installmentStructure,
+    {
+      id: installmentStructure.length,
+      installment: remain,
+      value: 0,
+    },
+  ]);
+
+  setPaidInstallment([...paidInstallment, { ...content }]);
+}
+
+// Adjust in next Installment Feature
+function addjustInNextInstallment(
+  next,
+  content,
+  installmentStructure,
+  setInstallmentStructure,
+  i,
+  setPaidInstallment,
+  paidInstallment
+) {
+  let elementNext = next[i + 1];
+
+  let remain = content.installment - content.value;
+  elementNext.installment = elementNext.installment + remain;
+  installmentStructure.splice(i, 1);
+  setInstallmentStructure([...installmentStructure]);
+  setPaidInstallment([...paidInstallment, { ...content }]);
 }
